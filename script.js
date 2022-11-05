3bc7c8b88cb40b10f6bea88cb97fb4ed4b5e64b5
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2022-11-05 12:14:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,106 @@
+const quizData = [
+    {
+        question: 'How old is Ulugbek?',
+        a: '15',
+        b: '37',
+        c: '28',
+        d: '40',
+        correct: 'c'
+    }, {
+        question: 'What is the most used programming language in 2022?',
+        a: 'Java',
+        b: 'Python',
+        c: 'Javascript',
+        d: 'C',
+        correct: 'c'
+    }, {
+        question: 'Who is the President of US?',
+        a: 'Ulugbek Kobulov',
+        b: 'Donald Trump',
+        c: 'Ivan Saldano',
+        d: 'Barack Obama',
+        correct: 'a'
+    }, {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Cascading Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Application Programming Interface',
+        correct: 'a'
+    }, {
+        question: 'What year was JavaScript',
+        a: '1996',
+        b: '1995',
+        c: '1997',
+        d: 'none of the above',
+        correct: 'b'
+    }
+];
+
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
+
+
+loadQuiz();
+
+function loadQuiz() {
+
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+
+    
+}
+
+function getSelected(){
+    
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+       if(answerEl.checked){
+            answer = answerEl.id;
+       }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+     answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener("click", () => {
+
+    const answer = getSelected();
+
+    if(answer){
+        if(answer === quizData[currentQuiz].correct){
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();        
+        } else {
+            quiz.innerHTML =`<h2> You answered correctly at ${score}/${quizData.length} questions.</h2> <button onclick="location.reload()">Reload</button>`;
+        }
+    }
+});
+
